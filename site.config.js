ce99b7d9d3845627c476eb8b8cf1c626275babb5
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:53:52 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: '정보이용료미납 희망티켓',
+  siteUrl: 'https://contents-hh-7.pages.dev',
+  siteDescription: '정보이용료미납 걱정 끝! 통신사 정책 제한 및 결제 오류 코드를 정식 업체가 안전하고 빠르게 풀어 드립니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
