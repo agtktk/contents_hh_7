a4c24c06ca75d7cef64e1539d70bbd3ad5ed30cf
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:49:53 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: '정보이용료미납 걱정 끝! 통신사 정책 제한 및 결제 오류 코드를 정식 업체가 안전하고 빠르게 풀어 드립니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: '2397adff59968fe88d03b87080a3a8a5a6f96b61',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
